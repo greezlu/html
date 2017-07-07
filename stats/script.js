982bb40901c9e4a1dd5ad9ce4b6696ef6458c7fc
greezlu <greezlu@gmail.com>
2017-07-07 10:50:58 +0300

    Update script.js

--- stats/script.js
+++ stats/script.js
@@ -1,24 +1,3 @@
-// List of games
-var gameList = {
-	"Black Desert Online [EU]": 26130,
-	"Elite Dangerous": 6862,
-	"EVE Online": 2836,
-	"Aion": 1177,
-	"Final Fantsy XIV": 20131,
-	"The Elder Scrolls Online": 12378,
-	"TERA": 2665,
-	"TERA [RU]": 646,
-	"Warframe": 40813,
-	"Player Unknown Battlegrounds": 276837,
-	"Rust": 39812,
-	"Rocket League": 64611,
-	"Arma 3": 40790,
-	"Rift": 647,
-	"Worlds Adrift": 798,
-	"Skyforge": 695,
-	"Grand Theft Auto 5": 100886
-};
-
 var sample;
 var container = document.getElementsByClassName('container')[0];
 var main = document.getElementsByClassName('main')[0];
