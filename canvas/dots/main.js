6e7138eea122a7ab8e556062430554557258b6c2
greezlu <greezlu@gmail.com>
2017-08-09 03:14:14 +0300

    Create main.js

---
+++ canvas/dots/main.js
@@ -0,0 +1,48 @@
+var canvas = document.getElementById('mainCanvas');
+var context = canvas.getContext('2d');
+
+var qnt = 800;
+
+(function () {
+
+	var currentHeight,
+			currentWidth,
+			resizing = null;
+
+	resizeCanvas();
+	window.addEventListener('resize', onResize);
+
+	function onResize (e) {
+		clearTimeout(resizing);
+		resizing = setTimeout(resizeCanvas, 300);
+	};
+
+	function resizeCanvas () {
+		currentHeight = document.documentElement.clientHeight;
+		currentWidth = document.documentElement.clientWidth;
+
+		canvas.setAttribute('height', currentHeight);
+		canvas.setAttribute('width', currentWidth);
+	};
+
+})();
+
+Circle.prototype.canvas = canvas;
+Circle.prototype.context = context;
+
+Circle.prototype.colorArray = [
+	'#2C3E50',
+	'#E74C3C',
+	'#ECF0F1',
+	'#3498DB',
+	'#2980B9'
+];
+
+for (var i=qnt; i--; i) {
+	new Circle({
+		radius: 15,
+		maxRadius: 50,
+		remote: 100} );
+};
+
+Circle.prototype.animate();
