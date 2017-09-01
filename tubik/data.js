fe008e919079870267204eb383b17f65c7006987
greezlu <greezlu@gmail.com>
2017-09-01 20:01:41 +0300

    Create data.js

---
+++ tubik/data.js
@@ -0,0 +1,134 @@
+var data = {
+	GBR: {
+		name: "United Kingdom",
+		importData: {
+			transit: "1 / 2 days",
+			week: ["mon", "tue", "wed", "thu", "fri"]
+		},
+		exportData: {
+			transit: "1 / 2 days",
+			week: ["mon", "tue", "wed", "thu", "fri"]
+		}
+	},
+	BEL: {
+		name: "Belgium",
+		importData: {
+			transit: "2 - 3 days",
+			week: ["mon", "tue", "wed", "fri"]
+		},
+		exportData: {
+			transit: "2 - 3 days",
+			week: ["mon", "tue", "wed", "fri"]
+		}
+	},
+	CZE: {
+		name: "Czech",
+		importData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		},
+		exportData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		}
+	},
+	FRA: {
+		name: "France",
+		importData: {
+			transit: "2 - 3 days",
+			week: ["wed", "fri"]
+		},
+		exportData: {
+			transit: "2 - 3 days",
+			week: ["tue", "fri"]
+		}
+	},
+	DEU: {
+		name: "Germany",
+		importData: {
+			transit: "2 - 3 days",
+			week: ["tue", "wed", "fri"]
+		},
+		exportData: {
+			transit: "2 - 3 days",
+			week: ["tue", "fri"]
+		}
+	},
+	NLD: {
+		name: "Netherlands",
+		importData: {
+			transit: "2 - 3 days",
+			week: ["mon", "tue", "wed", "fri"]
+		},
+		exportData: {
+			transit: "2 - 3 days",
+			week: ["mon", "tue", "wed", "fri"]
+		}
+	},
+	HUN: {
+		name: "Hungary",
+		importData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		},
+		exportData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		}
+	},
+	ITA: {
+		name: "Italy",
+		importData: {
+			transit: "3 days",
+			week: ["tue", "fri"]
+		},
+		exportData: {
+			transit: "3 days",
+			week: ["tue", "fri"]
+		}
+	},
+	POL: {
+		name: "Poland",
+		importData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		},
+		exportData: {
+			transit: "4+ days",
+			week: ["tue", "fri"]
+		}
+	},
+	PRT: {
+		name: "Portugal",
+		importData: {
+			transit: "3 - 4 days",
+			week: ["fri"]
+		},
+		exportData: {
+			transit: "3 - 4 days",
+			week: ["fri"]
+		}
+	},
+	ESP: {
+		name: "Spain",
+		importData: {
+			transit: "3 - 4 days",
+			week: ["fri"]
+		},
+		exportData: {
+			transit: "3 - 4 days",
+			week: ["fri"]
+		}
+	},
+	TUR: {
+		name: "Turkey",
+		importData: {
+			transit: "10 - 12 days",
+			week: ["tue", "fri"]
+		},
+		exportData: {
+			transit: "10 - 12 days",
+			week: ["tue", "fri"]
+		}
+	}
+};
