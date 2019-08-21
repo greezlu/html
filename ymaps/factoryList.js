5bf4ec6ce693bda611748643b6ca3ada2327e26a
greezlu <greezlu@gmail.com>
2019-08-22 00:01:32 +0300

    Add files via upload

---
+++ ymaps/factoryList.js
@@ -0,0 +1,203 @@
+var factoryList = [
+
+  {
+    name: "ООО АтлантБетон",
+    adressList: ["ул. Алексеевская, 1", "Дзержинский, Московская область, Россия, 140090"],
+    contactPhoneList: ["+7 (495) 107-92-34", "+7 (495) 150-46-85", "+7 (903) 795-92-02", "+7 (903) 960-85-71"],
+    contactEmailList: ["info@atlantbeton.ru"],
+    coords: [55.628124, 37.803569],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО АтлантБетон",
+    adressList: ["Огородный проезд, 2", "Москва, Россия, 127254"],
+    contactPhoneList: ["+7 (495) 107-92-34", "+7 (495) 150-46-85", "+7 (903) 795-92-02", "+7 (903) 960-85-71"],
+    contactEmailList: ["info@atlantbeton.ru"],
+    coords: [55.809912, 37.610107],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО АтлантБетон",
+    adressList: ["Рязанский проспект, вл26", "Москва, Россия"],
+    contactPhoneList: ["+7 (495) 107-92-34", "+7 (495) 150-46-85", "+7 (903) 795-92-02", "+7 (903) 960-85-71"],
+    contactEmailList: ["info@atlantbeton.ru"],
+    coords: [55.716789, 37.769690],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО АтлантБетон",
+    adressList: ["посёлок завода Мосрентген, 33", "Москва, Россия"],
+    contactPhoneList: ["+7 (495) 107-92-34", "+7 (495) 150-46-85", "+7 (903) 795-92-02", "+7 (903) 960-85-71"],
+    contactEmailList: ["info@atlantbeton.ru"],
+    coords: [55.620298, 37.468187],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО ПКР",
+    adressList: ["Фабричный проезд, 4", "микрорайон Климовск, Подольск, Московская область, Россия, 142180"],
+    contactPhoneList: ["8(926)703-95-45", "8(499)397-88-50"],
+    contactEmailList: ["a12.n@yandex.ru"],
+    coords: [55.346439, 37.504543],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Бетонный завод Курмис",
+    adressList: ["улица Жуковского, 3", "Долгопрудный, Московская область, Россия, 141707"],
+    contactPhoneList: ["+7(999)895-32-79"],
+    contactEmailList: ["betonmpk@mail.ru"],
+    coords: [55.925874, 37.518778],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Монолит Бетон",
+    adressList: ["сельское поселение Кузнецовское", "Раменский городской округ, Московская область, Россия"],
+    contactPhoneList: ["8(495)131-47-13", "8(916)499-29-99"],
+    contactEmailList: ["betonpanov@yandex.ru"],
+    coords: [55.457126, 38.306295],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО МОСАВТОБЕТОН-ГМ",
+    adressList: ["проспект Мира, 36А", "Реутов, Московская область, Россия, 143960"],
+    contactPhoneList: ["+7(967)055-55-85", "+7(966)153-33-31"],
+    contactEmailList: ["mosavtobeton@mail.ru"],
+    coords: [55.770564, 37.868512],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО МОСАВТОБЕТОН-ГМ",
+    adressList: ["Чермянская улица, 1с1", "Москва, Россия, 127282"],
+    contactPhoneList: ["+7(967)055-55-85", "+7(966)153-33-31"],
+    contactEmailList: ["mosavtobeton@mail.ru"],
+    coords: [55.884476, 37.634852],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО МОСАВТОБЕТОН-ГМ",
+    adressList: ["улица Кирпичные Выемки, 14с4", "Москва, Россия, 117405"],
+    contactPhoneList: ["+7(967)055-55-85", "+7(966)153-33-31"],
+    contactEmailList: ["mosavtobeton@mail.ru"],
+    coords: [55.583860, 37.611031],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО МОСАВТОБЕТОН-ГМ",
+    adressList: ["1-й Южнопортовый проезд, 15Ас2", "Москва, Россия, 115088"],
+    contactPhoneList: ["+7(967)055-55-85", "+7(966)153-33-31"],
+    contactEmailList: ["mosavtobeton@mail.ru"],
+    coords: [55.711471, 37.705292],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО МонолитКомплектСервис",
+    adressList: ["Нижегородская улица, 29", "Москва, Россия, 109052"],
+    contactPhoneList: ["8(495)941-95-50"],
+    contactEmailList: ["mks1105@mail.ru"],
+    coords: [55.734016, 37.710765],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Хоумстрой",
+    adressList: ["Нагорный проезд, 10с3", "Москва, Россия, 117105"],
+    contactPhoneList: ["+7(499)648-82-92"],
+    contactEmailList: ["info@torgbet.ru"],
+    coords: [55.687625, 37.613129],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Евробетон плюс",
+    adressList: ["микрорайон Пронина, 5", "Звенигород, Одинцовский городской округ, Московская область, Россия"],
+    contactPhoneList: ["+7(910)404-06-14"],
+    contactEmailList: ["1009252@mail.ru"],
+    coords: [55.741897, 36.856364],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Мастерстрой",
+    adressList: ["посёлок Архангельское, 2Бс2", "городской округ Красногорск, Московская область, Россия"],
+    contactPhoneList: ["+8(495)768-87-35"],
+    contactEmailList: ["masterstroy-info@mail.ru"],
+    coords: [55.790945, 37.296390],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО Абсолют Бетон",
+    adressList: ["Очаковское шоссе, 5А", "Москва, Россия, 119530"],
+    contactPhoneList: ["+7(910)450-88-32"],
+    contactEmailList: ["abtbeton@mail.ru"],
+    coords: [55.695024, 37.460183],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО ОВИРАСТРОЙ-БЕТОН",
+    adressList: ["улица Верхние Поля, 65с3", "Москва, Россия, 109429"],
+    contactPhoneList: ["+7(967)250-11-55"],
+    contactEmailList: ["os-beton@yandex.ru"],
+    coords: [55.659024, 37.805396],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО ТЕХНОЛОГИИ БЕТОНА",
+    adressList: ["улица Кржижановского, 15к1", "Москва, Россия, 117218"],
+    contactPhoneList: ["+7(967)250-11-55"],
+    contactEmailList: ["tehnologbeton@mail.ru"],
+    coords: [55.680277, 37.570653],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО НВЛ-ГРУПП",
+    adressList: ["1-я Северная линия, 1с16", "Москва, Россия, 127204"],
+    contactPhoneList: ["8(925)521-55-55"],
+    contactEmailList: ["sale@nwl-group.ru"],
+    coords: [55.940337, 37.565997],
+    productList: [],
+    deliveryPrice: ""
+  },
+
+  {
+    name: "ООО БетонАктив",
+    adressList: ["Передельцевская улица, 35с1", "деревня Саларьево, поселение Московский, Москва, Россия"],
+    contactPhoneList: ["8(985)761-32-20"],
+    contactEmailList: ["betonaktiv@mail.ru"],
+    coords: [55.617570, 37.426767],
+    productList: [],
+    deliveryPrice: ""
+  }
+
+]
